2b02825e3b93c8be43c2e1b255a7605f5c055d66
Nicholas Assaderaghi <nra2130@columbia.edu>
2024-11-05 02:25:58 -0500

    Revamped test cases and reformatted code.

--- Assembler/assembler.py
+++ Assembler/assembler.py
@@ -1,9 +1,9 @@
 import argparse
 from definitions import *
 
-# guide for my custom assembly language: bitly/nra2130Assembly101
+# guide for my custom assembly language: bit.ly/nra2130Assembly101
 
-errors = [] # list of errors that occurred during the assembly process
+errors = [] # list of string descriptions of errors that occurred during the assembly process
 
 def check_address(address: str, address_type: Input) -> bool:
     """Check if the inputted address is a valid register, ram address, or line number (to jump to)."""
@@ -18,14 +18,14 @@ def check_address(address: str, address_type: Input) -> bool:
     return address_type.within_bounds(val)
 
 def check_integer(number: str) -> bool:
-    """Check if the inputted number is a valid integer literal."""
+    """Check if the inputted number is a valid integer immediate."""
     try:
         val = int(number)
     except ValueError:
         return False
     return Input.INTGR.within_bounds(val)
 
-def error(input: Operation | str, line_num: int, reason = "None") -> str:
+def error(input: Operation | str, line_num: int, reason = "none") -> str:
     """Generic error message creator for invalid operations."""
     errors.append(f"Line #{line_num} is invalid, because '{input}' is invalid. Specified reason: {reason}.")
     return "ERROR"
@@ -38,29 +38,38 @@ def input_amount_error(line_num: int, expct_amnt: int, actl_amnt: int) -> str:
     return "ERROR"
 
 
-# test cases for the different aseembly operations, key is the command,
-# value is "VALUE" if the command is valid or "ERROR" if the operation is invalid
+# some test cases for the different aseembly operations
+
 test_cases = {
-    "_add_ rg4 rg5 rg3": "VALUE",
-    "_sub_": "ERROR", # no inputs
-    "_cjp_ ln5 rg5 rg3 _and_": "VALUE",
-    "_cjp_ ln12345 rg15 rg0 _xor_": "VALUE",
-    "_cjp_ ln98765 rg15 rg16 _xor_": "ERROR", # too big of a line number
-    "_sub_ rm5": "ERROR", # too few inputs and wrong type of input
-    "_grt_ rg5 rg6": "ERROR", # too few inputs
-    "_eql_ rg1 rg0 rg2": "VALUE",
-    "_jmp_ ln": "ERROR", # missing an integer literal after ln
-    "_rst_ rm5 00": "ERROR", # wrong type of input and 00 is not a valid integer literal
-    "_rrd_ gr5 gr5": "ERROR", # wrong type of input (not even a valid input)
+    "_add_ rg1 rg2 rg3": "VALUE",
+    "_sub_ rg4 rg5 rg6": "VALUE",
+    "_grt_ rg7 rg8 rg9": "VALUE",
+    "_eql_ rg10 rg11 rg12": "VALUE",
+    "_jmp_ ln0": "VALUE",
+    "_cjp_ ln65535 rg13 rg14 _and_": "VALUE",
+    "_rst_ rg15 12345": "VALUE",
+    "_rrd_ rg14 rg13": "VALUE",
     "_rcl_": "VALUE",
-    "_and_ rg5 rm5 gr4": "ERROR", # wrong type of input (not even a valid input)
-    "_bor_ rg5 rg9 rg11": "VALUE",
-    "_xor_ rm5 rg5 rg6": "ERROR", # wrong type of input
-    "_not_ rg5 rg4 rg3": "ERROR", # too many inputs
-    "_nat_ rg5 rg4": "ERROR", # not a valid operation
-    "_rld_ rm255 rg6": "VALUE",
-    "_rms_ rm256 rg 16": "ERROR", # ram adress is too large
-    "_inv_ rg15 rg14": "VALUE"
+    "_and_ rg12 rg11 rg10": "VALUE",
+    "_bor_ rg9 rg8 rg7": "VALUE",
+    "_xor_ rg6 rg5 rg4": "VALUE",
+    "_not_ rg3 rg1": "VALUE",
+    "_rld_ rm255 rg0": "VALUE",
+    "_rms_ rm0 rg15": "VALUE",
+    "_inv_ rg0 rg0": "VALUE",
+    "_sub_": "ERROR",                      # missing three inputs
+    "_jmp_ ln65536": "ERROR",              # too large of a line number
+    "_cjp_ ln0 rg13 rg14 _add_": "ERROR",  # invalid comparison operation for conditional jump
+    "_sub_ rm100 rg5 rg4": "ERROR",        # wrong type of input, should be register not RAM address
+    "_grt_ rg5 rg6": "ERROR",              # missing one input
+    "_jmp_ ln": "ERROR",                   # missing an integer immediate after ln (invalid input)
+    "_rst_ rm5 0": "ERROR",                # wrong type of input, should be register not RAM address
+    "_rrd_ gr5 gr5": "ERROR",              # wrong type of input (invalid input prefix)
+    "_and_ rg5 rg5 gr4": "ERROR",          # wrong type of input (invalid input prefix)
+    "_xor_ rg5 rg4 rg16": "ERROR",         # too large of a register address
+    "_not_ rg5 rg4 rg3": "ERROR",          # too many inputs
+    "_nat_ rg5 rg4": "VALUE",              # invalid operation
+    "_rms_ rm256 rg15": "ERROR",           # too large of a RAM address
 }
 
 parser = argparse.ArgumentParser(description='Assemble your assembly code into binary!')
@@ -75,49 +84,47 @@ if __name__ == "__main__":
     assembly_code = []
     if testing:
         print("Running test cases...")
-        for test_op in test_cases.keys():
-            assembly_code.append(test_op)
+        assembly_code = list(test_cases)
     elif input_file:
         with open(input_file, "r") as f:
             assembly_code = f.readlines()
     else:
-        print("Please enter your line of assembly code:", end=" ")
-        assembly_code.append(input())
+        assembly_code = [input("Please enter your line of assembly code: ")]
 
     assembled_code = [] # the final assembled code, each element is a line of binary code (as a string)
-    line_num = 0
 
-    for line in assembly_code:
-        line_num += 1
+    for line_num, line in enumerate(assembly_code, 1):
         inputs = line.split()
 
         try:
             operation = inputs.pop(0)
         except IndexError:
-            assembled_code.append(error("", line_num, "no operation"))
+            assembled_code.append("ERROR")
+            error("", line_num, "no operation")
             continue
 
         try:
             operation = Operation(operation)
         except ValueError:
-            assembled_code.append(error(operation, line_num, "invalid operation"))
+            assembled_code.append("ERROR")
+            error(operation, line_num, "invalid operation")
             continue
 
         binary = operation.bnry
 
         expctd_amnt = len(operation.inputs)
         actl_amnt = len(inputs)
-        if actl_amnt > expctd_amnt or actl_amnt < expctd_amnt:
-            assembled_code.append(input_amount_error(line_num, expctd_amnt, actl_amnt))
+        if actl_amnt != expctd_amnt:
+            assembled_code.append("ERROR")
+            input_amount_error(line_num, expctd_amnt, actl_amnt)
             continue
-        index = 0
-        # the following for loop checks the validity of the inputs depending on the operation
-        for input in inputs:
-            input_type = operation.inputs[index]
+        # check the validity of the inputs depending on the operation
+        for input, input_type in zip(inputs, operation.inputs):
             if input_type == Input.RG:
                 binary += f'{int(input[2:]):04b}' if check_address(input, Input.RG) else error(input, line_num, "invalid register address")
             elif input_type == Input.INTGR:
                 # https://stackoverflow.com/questions/63274885/converting-an-integer-to-signed-2s-complement-binary-string
+                # bitmask to grab the last 16 bits of the integer
                 binary += f'{int(input) & ((1 << 16) - 1):016b}' if check_integer(input) else error(input, line_num, "invalid number")
             elif input_type == Input.LN:
                 binary += f'{int(input[2:]):016b}' if check_address(input, Input.LN) else error(input, line_num, "invalid line number")
@@ -125,14 +132,13 @@ if __name__ == "__main__":
                 binary += f'{int(input[2:]):08b}' if check_address(input, Input.RM) else error(input, line_num, "invalid ram address")
             elif input_type == Input.BTWSE:
                 try:
-                    input = Operation(input)
+                    input = Operation(input) # this may throw a ValueError
                     if not input.is_btwse_cmps_op():
                         raise ValueError
                     binary += input.bnry
                 except ValueError:
                     binary += error(input, line_num, "invalid bitwise operation")
 
-            index += 1
 
         # length of binary code should be 32, we need to add zero padding if it's not
         padding = 32 - len(binary)
@@ -146,17 +152,22 @@ if __name__ == "__main__":
         else:
             assembled_code.append("ERROR")
 
-    print('\n'+'\n'.join(errors))
-    print("\nOutput: {}".format(assembled_code))
+    if errors:
+        print('\n'+'\n'.join(errors))
+    if not testing:
+        print("\nOutput:\n" + '\n'.join(assembled_code))
 
     if input_file:
         with open(args["output"], "w+") as output:
-            output.write("\nOutput: {}".format(assembled_code))
+            for code in assembled_code:
+                output.write(f"{code}\n")
 
     if testing:
         expected_array = list(test_cases.values())
-        print("Expctd: {}".format(expected_array))
         if expected_array == assembled_code:
             print("\nTest Results: Assembler working properly 😊")
         else:
+            for i, (line, expected, actual) in enumerate(zip(line, expected_array, assembled_code)):
+                if expected != actual:
+                    print(f"Failed case #{i}: {line}")
             print("\nTest Results: Assembler working improperly 🫠")
--- Assembler/definitions.py
+++ Assembler/definitions.py
@@ -2,28 +2,18 @@ from enum import StrEnum, auto
 
 # https://jwodder.github.io/kbits/posts/multi-value-enum/
 
-# While the docstrings throughout do interrupt the code flow, they provide helpful documentation and intellisense.
-# However, I may change my mind in the future about the tradeoff between readability and documentation, and replace them with inline comments.
 class Input(StrEnum):
     """Assembly input types and their bounds."""
     
-    RG = (auto(), 0, 2**4)
-    """Register address, unsigned 4-bit"""
-    RM = (auto(), 0, 2**8)
-    """Ram address, unsigned 8-bit"""
-    LN = (auto(), 0, 2**16)
-    """Line number, unsigned 16-bit, used for jumps"""
-    INTGR = (auto(), -(2**15), 2**15)
-    """Integer value, signed 16-bit, RAM and registers hold these values"""
-    BTWSE = (auto(), None, None) 
-    """Comparison bitwise operation, used for conditional jumps"""
+    RG = (auto(), 0, 2**4)             # register address, unsigned 4-bit
+    RM = (auto(), 0, 2**8)             # RAM address, unsigned 8-bit
+    LN = (auto(), 0, 2**16)            # line number, unsigned 16-bit, used for jumps
+    INTGR = (auto(), -(2**15), 2**15)  # integer immediate, signed 16-bit, RAM and registers hold these values
+    BTWSE = (auto(), None, None)       # comparison bitwise operation, used for conditional jumps
     
-    value: str
-    """String representation of the input type, used for validation"""
-    min_: int
-    """Minimum value, inclusive"""
-    max_: int
-    """Maximum value, exclusive"""
+    _value_: str  # string representation of the input type, used for validation
+    min_: int     # min value, inclusive
+    max_: int     # max value, exclusive
     
     def __new__(cls, value: str, min_: int, max_: int) -> "Input":
         obj = str.__new__(cls, value)
@@ -40,45 +30,26 @@ class Input(StrEnum):
 class Operation(StrEnum):
     """Assembly operations, their binary representations, and required inputs."""
     
-    ADD = ("_add_", "0000", [Input.RG, Input.RG, Input.RG])
-    """Addition"""
-    SUB = ("_sub_", "0001", [Input.RG, Input.RG, Input.RG])
-    """Subtraction"""
-    GRT = ("_grt_", "0010", [Input.RG, Input.RG, Input.RG])
-    """Greater than (comparison)"""
-    EQL = ("_eql_", "0011", [Input.RG, Input.RG, Input.RG])
-    """Equal to (comparison)"""
-    JMP = ("_jmp_", "0100", [Input.LN])
-    """Jump"""
-    CJP = ("_cjp_", "1111", [Input.LN, Input.RG, Input.RG, Input.BTWSE])
-    """Conditional jump"""
-    RST = ("_rst_", "0101", [Input.RG, Input.INTGR])
-    """Register set"""
-    RRD = ("_rrd_", "0110", [Input.RG, Input.RG])
-    """Register read"""
-    RCL = ("_rcl_", "0111", [])
-    """Register clear"""
-    AND = ("_and_", "1000", [Input.RG, Input.RG, Input.RG])
-    """Bitwise and"""
-    BOR = ("_bor_", "1001", [Input.RG, Input.RG, Input.RG])
-    """Bitwise or"""
-    XOR = ("_xor_", "1010", [Input.RG, Input.RG, Input.RG])
-    """Bitwise xor"""
-    NOT = ("_not_", "1011", [Input.RG, Input.RG])
-    """Bitwise not"""
-    RLD = ("_rld_", "1100", [Input.RG, Input.RG])
-    """Register load (write from ram to register)"""
-    RMS = ("_rms_", "1101", [Input.RG, Input.RG])
-    """Ram set."""
-    INV = ("_inv_", "1110", [Input.RG, Input.RG])
-    """Bitwise invert (negate?)."""
+    ADD = ("_add_", "0000", [Input.RG, Input.RG, Input.RG])              # addition
+    SUB = ("_sub_", "0001", [Input.RG, Input.RG, Input.RG])              # subtraction
+    GRT = ("_grt_", "0010", [Input.RG, Input.RG, Input.RG])              # greater than
+    EQL = ("_eql_", "0011", [Input.RG, Input.RG, Input.RG])              # equal to
+    JMP = ("_jmp_", "0100", [Input.LN])                                  # jump
+    CJP = ("_cjp_", "1111", [Input.LN, Input.RG, Input.RG, Input.BTWSE]) # conditional jump
+    RST = ("_rst_", "0101", [Input.RG, Input.INTGR])                     # register set
+    RRD = ("_rrd_", "0110", [Input.RG, Input.RG])                        # register read
+    RCL = ("_rcl_", "0111", [])                                          # register clear
+    AND = ("_and_", "1000", [Input.RG, Input.RG, Input.RG])              # bitwise and
+    BOR = ("_bor_", "1001", [Input.RG, Input.RG, Input.RG])              # bitwise or
+    XOR = ("_xor_", "1010", [Input.RG, Input.RG, Input.RG])              # bitwise exclusive or
+    NOT = ("_not_", "1011", [Input.RG, Input.RG])                        # bitwise not
+    RLD = ("_rld_", "1100", [Input.RM, Input.RG])                        # register load (write from RAM to register)
+    RMS = ("_rms_", "1101", [Input.RM, Input.RG])                        # RAM set (write from register to RAM)
+    INV = ("_inv_", "1110", [Input.RG, Input.RG])                        # negate (bitwise not + 1)
     
-    value: str
-    """Assembly representation of operation."""
-    bnry: str
-    """Binary representation of operation."""
-    inputs: list[Input]
-    """Inputs required for operation."""
+    _value_: str         # assembly representation of operation
+    bnry: str            # binary representation of operation
+    inputs: list[Input]  # inputs required for operation
     
     def __new__(cls, value: str, bnry: str, inputs: list[Input]) -> "Operation":
         obj = str.__new__(cls, value)
